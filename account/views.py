dee54c293cbbc1ebb58b98a2465293395bf88d2e
Danson <githukadanson23@gmail.com>
2025-10-01 06:21:53 +0300

    fixed the authentication

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .forms import  MyUserCreationForm
+from django.contrib import messages
+from django.urls import reverse
+from .forms import MyUserCreationForm, UserSignInForm
 
 def signup(request):
     if request.user.is_authenticated:
@@ -11,13 +13,17 @@ def signup(request):
         if form.is_valid():
             user = form.save(commit=False)
             user.save()
-            username = form.cleaned_data.get('username') 
+            username = form.cleaned_data.get('username')
             login(request, user)
-            return redirect('/')
+            messages.success(request, f'Welcome to Campus Shoppy, {username}! Your account has been created successfully.')
+            # Redirect to next page or home
+            next_page = request.GET.get('next', '/')
+            return redirect(next_page)
         else:
+            messages.error(request, 'Please correct the errors below.')
             return render(request, 'account/signup.html', {'form': form})
     else:
-        form =  MyUserCreationForm()
+        form = MyUserCreationForm()
         return render(request, 'account/signup.html', {'form': form})
 
 
@@ -25,18 +31,29 @@ def signin(request):
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = UserSignInForm(request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {user.username}!')
+                # Redirect to next page or home
+                next_page = request.GET.get('next', '/')
+                return redirect(next_page)
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Please correct the errors below.')
         return render(request, 'account/signin.html', {'form': form})
     else:
-        form = AuthenticationForm(request)
+        form = UserSignInForm()
         return render(request, 'account/signin.html', {'form': form})
 
 
 def signout(request):
     logout(request)
+    messages.info(request, 'You have been logged out successfully.')
     return redirect('home')
 
